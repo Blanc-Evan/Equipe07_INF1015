25a1df590261bc9a70e94564f1b9fa107273dbf2
evan <evanblanc.pro@icloud.com>
2021-09-13 18:06:25 -0400

    Exo5_V2

--- Exo5/Source.cpp
+++ Exo5/Source.cpp
@@ -3,10 +3,11 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
-const string FILENAME = "inventaire.txt";
+const string FILENAME = "./inventaire.txt";
 const int TAILLE = 10;
 
 struct Aliment {
@@ -17,21 +18,46 @@ struct Aliment {
 	float prix = 0;
 };
 
+void afficherAlimentLePlusCher(vector<Aliment> inventaire);
 
 int main(int argc, char const* argv[]) {
-
-	Aliment tab[TAILLE];
+	
+	vector<Aliment> inventaire;
 	int i = 0;
 	ifstream file(FILENAME);
-	if (file.is_open()) {
+	if (file.is_open()) {	// Read File
 		string line;
 		while (getline(file, line)) {
 
 			istringstream str(line);
 			Aliment a;
 			str >> a.nom >> a.type >> a.quantite >> a.prix;
-			tab[i] = a;
+			inventaire[i] = a;
 			i++;
 		}
+		afficherAlimentLePlusCher(inventaire);
 	}
+	else {
+		cout << "Exception: File not found";
+	}
+
+
+}
+
+void afficherAlimentLePlusCher(vector<Aliment> inventaire) {
+
+	Aliment &lePlusCher = inventaire[0];
+
+	for (int i = 1; i < inventaire.size()-1; i++) 
+		if (inventaire[i].prix > lePlusCher.prix)
+			lePlusCher = inventaire[i];
+	
+	cout << lePlusCher.quantite
+			<< " " 
+			<< lePlusCher.nom
+			<< " (" 
+			<< lePlusCher.type
+			<< ""
+			<< lePlusCher.prix
+			<< "$ chacun";
 }
