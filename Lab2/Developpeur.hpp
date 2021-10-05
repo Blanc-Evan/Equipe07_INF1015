0eb922e0aa41436cbe56fe70dec5c5ede9972aed
Evan Joël Régis BLANC <51710175+Blanc-Evan@users.noreply.github.com>
2021-10-05 19:11:27 -0400

    Refactor Developpeur

--- Lab2/Developpeur.hpp
+++ Lab2/Developpeur.hpp
@@ -22,83 +22,84 @@ public:
 
 private:
 	pair<std::string, ListeJeux> paireNomJeux_;
-	void augmenterTaille(ListeJeux& l, const int newCapacity);
-
-
-	Developpeur(const string& nom = "unknown") {
-
-		this->paireNomJeux_.first = nom;
-		
-		ListeJeux* l = new ListeJeux();
-		l->capacite = 1;
-		l->nElements = 0;
-		l->elements = new Jeu*;
-		
-		this->paireNomJeux_.second = *l;
-	}
-
-
-	const string& getName() const {
-
-		return this->paireNomJeux_.first;
-	}
-
-
-	const int howManyGamesDidHeParticipatesToInList(const ListeJeux& l) const {
-
-		int n = 0;
-		for (int i : iter::range(l.nElements)) {
-			if (l.elements[i]->developpeur == this->getName()) {
-				n++;
-			}
-		}
-		return n;
-	}
-
-
-	void updateListeJeux(ListeJeux& l) {
-
-		const int i = this->howManyGamesDidHeParticipatesToInList(l);
-
-		if (this->paireNomJeux_.second.capacite <= this->paireNomJeux_.second.capacite + i) {
-			augmenterTaille(this->paireNomJeux_.second, i);
-		}
-
-		for (int j : iter::range(l.nElements)) {
-			if (l.elements[j]->developpeur == this->getName()) {
-				this->paireNomJeux_.second.elements[j] = l.elements[j];
-			}
-		}
-	}
-
-
-	void augmenterTaille(ListeJeux& l, const int newCapacity) const {
-
-		if (newCapacity != 0) {
-			l.capacite += newCapacity;
-			Jeu** temp = new Jeu* [l.capacite];
-
-			for (auto i : iter::range(l.nElements)) {
-				temp[i] = l.elements[i];
-				delete[] l.elements[i];
-			}
-
-			l.elements = temp;
-		}
-	}
-
-
-	void printGames() const {
-
-		for (int i : iter::range(this->paireNomJeux_.second.nElements)) {
-			cout << this->paireNomJeux_.second.elements[i]->titre + "\n";
-		}
-	}
-
-	~Developpeur() {
-
-		delete[] this->paireNomJeux_.second.elements;
-		this->paireNomJeux_.second.elements = 0;
-		delete &this->paireNomJeux_.second;
-	}
+	void augmenterTaille(ListeJeux& l, const int newCapacity) const;
 };
+
+
+Developpeur::Developpeur::Developpeur(const string& nom = "unknown") {
+
+	this->paireNomJeux_.first = nom;
+
+	ListeJeux* l = new ListeJeux();
+	l->capacite = 1;
+	l->nElements = 0;
+	l->elements = new Jeu*;
+
+	this->paireNomJeux_.second = *l;
+}
+
+
+const string& Developpeur::getName() const {
+
+	return this->paireNomJeux_.first;
+}
+
+
+const int Developpeur::howManyGamesDidHeParticipatesToInList(const ListeJeux& l) const {
+
+	int n = 0;
+	for (int i : iter::range(l.nElements)) {
+		if (l.elements[i]->developpeur == this->getName()) {
+			n++;
+		}
+	}
+	return n;
+}
+
+
+void Developpeur::updateListeJeux(ListeJeux& l) {
+
+	const int i = this->howManyGamesDidHeParticipatesToInList(l);
+
+	if (this->paireNomJeux_.second.capacite <= this->paireNomJeux_.second.capacite + i) {
+		augmenterTaille(this->paireNomJeux_.second, i);
+	}
+
+	for (int j : iter::range(l.nElements)) {
+		if (l.elements[j]->developpeur == this->getName()) {
+			this->paireNomJeux_.second.elements[j] = l.elements[j];
+		}
+	}
+}
+
+
+void Developpeur::augmenterTaille(ListeJeux& l, int newCapacity) const {
+
+	if (newCapacity != 0) {
+		l.capacite += newCapacity;
+		Jeu** temp = new Jeu * [l.capacite];
+
+		for (auto i : iter::range(l.nElements)) {
+			temp[i] = l.elements[i];
+			delete[] l.elements[i];
+		}
+
+		l.elements = temp;
+	}
+}
+
+
+void Developpeur::printGames() const {
+
+	for (int i : iter::range(this->paireNomJeux_.second.nElements)) {
+		cout << this->paireNomJeux_.second.elements[i]->titre + "\n";
+	}
+}
+
+
+Developpeur::~Developpeur() {
+
+	delete[] this->paireNomJeux_.second.elements;
+	this->paireNomJeux_.second.elements = 0;
+	delete& this->paireNomJeux_.second;
+}
